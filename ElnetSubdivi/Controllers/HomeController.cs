d18e1e6af543067f29d47f667cc273201aac7e4d
AmaroKobe03 <148180353+AmaroKobe03@users.noreply.github.com>
2025-02-26 09:33:13 +0800

    with integrated tailwindcss

---
+++ ElnetSubdivi/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ElnetSubdivi.Models;
+
+namespace ElnetSubdivi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Landing()
+        {
+            ViewData["HideLayout"] = true;
+            return View();
+        }
+        public IActionResult DashboardLayout()
+        {
+            return View();
+        }
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+    }
+
+
+}
